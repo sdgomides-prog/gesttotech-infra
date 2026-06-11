8f5755da271ab338c76daca84d901832d2233a11
sdgomides-prog <sdgomides@gmail.com>
2026-06-11 15:01:29 -0300

    feat(kore): add kore_payout.py controller
    Add initial KorePayout class definition.

---
+++ custom-apps/kore-pagamento/kore/kore_payout/kore_payout.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, GesttoTech
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class KorePayout(Document):
+    pass
