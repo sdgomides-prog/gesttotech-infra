1b7796c722d8f845c74a06fcbe65b450dcdf5918
sdgomides-prog <sdgomides@gmail.com>
2026-06-11 10:48:13 -0300

    feat(kore): add kore/hooks.py
    Initialize the Kore Pagamento app with basic metadata.

---
+++ custom-apps/kore-pagamento/kore/hooks.py
@@ -0,0 +1,10 @@
+app_name = "kore"
+app_title = "Kore Pagamento"
+app_publisher = "GesttoTech"
+app_description = "PIX via HappyPay para Frappe/ERPNext"
+app_version = "0.1.0"
+app_email = "dev@gesttotech.com.br"
+app_license = "MIT"
+
+doc_events = {}
+override_whitelisted_methods = {}
